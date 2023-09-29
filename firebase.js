c7f193d56c272a4ea242576736f1cc15395a12db
Jad Saade <sjad@MacBook-Air-de-Jad.local>
2023-09-29 10:49:59 +0200

    fistr

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from 'firebase/app'
+
+const app = initializeApp({
+  apiKey: "AIzaSyC502381zi66YA2hgQ0_rpHPOxvv8dD_Pk",
+  authDomain: "cloud-cours-22856.firebaseapp.com", 
+  projectId: "cloud-cours-22856",
+})
+
+export default app
